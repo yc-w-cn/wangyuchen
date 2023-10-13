a070fade1ca0c32ea7aab4374245c48924a116dd
YC.W <contact@wangyuchen.cn>
2023-10-13 14:28:29 +0800

    perf: class order

--- apps/web/app/contact/page.tsx
+++ apps/web/app/contact/page.tsx
@@ -5,7 +5,7 @@ export default function ContactPage() {
     <section className="mx-auto flex h-[calc(100vh-120px)] flex-col items-center justify-center gap-y-10 py-5">
       <div className="text-md flex flex-col gap-y-2">
         <Image src="/images/qrcode.jpg" width={300} height={300} alt="wx: joydragon"  />
-        <span className="text-gray-500 text-sm text-center font-bold">扫一扫上面的二维码图案，加我为朋友</span>
+        <span className="text-center text-sm font-bold text-gray-500">扫一扫上面的二维码图案，加我为朋友</span>
       </div>
     </section>
   )
