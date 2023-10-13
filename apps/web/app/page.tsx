942d86e19d84e4394bdecaa4ecf0ba806bc88504
YC.W <contact@wangyuchen.cn>
2023-10-13 13:40:21 +0800

    chore: update home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,38 +1,21 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import Image from "next/image"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+    <section className="mx-auto flex flex-col sm:flex-row h-[calc(100vh-120px)] items-center justify-center gap-y-10">
+      <div className="px-10">
+        <Image src="/images/avatar.png" width={200} height={200} alt="Avatar" />
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
+      <div className="flex flex-col items-center sm:items-start gap-2">
+        <h1 className="mt-3 text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Yuchen Wang
+        </h1>
+        <h2 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-3xl">
+          全栈软件工程师
+        </h2>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          探索前沿技术为业务赋能
+        </p>
       </div>
     </section>
   )
