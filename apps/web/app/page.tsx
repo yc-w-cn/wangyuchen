8757ad02c3cb00f6be2b06b39b1d13c60dec7c6c
YC.W <contact@wangyuchen.cn>
2023-10-13 13:41:17 +0800

    perf: class order

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image"
 
 export default function IndexPage() {
   return (
-    <section className="mx-auto flex flex-col sm:flex-row h-[calc(100vh-120px)] items-center justify-center gap-y-10">
+    <section className="mx-auto flex h-[calc(100vh-120px)] flex-col items-center justify-center gap-y-10 sm:flex-row">
       <div className="px-10">
         <Image src="/images/avatar.png" width={200} height={200} alt="Avatar" />
       </div>
-      <div className="flex flex-col items-center sm:items-start gap-2">
+      <div className="flex flex-col items-center gap-2 sm:items-start">
         <h1 className="mt-3 text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Yuchen Wang
         </h1>
