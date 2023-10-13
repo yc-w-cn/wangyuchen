f886ddfa37afb4786d701f33a05683baf64f20ce
YC.W <contact@wangyuchen.cn>
2023-10-13 14:26:19 +0800

    chore: create ContactPage

---
+++ apps/web/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+export default function ContactPage() {
+  return (
+    <section className="mx-auto flex h-[calc(100vh-120px)] flex-col items-center justify-center gap-y-10 py-5">
+      <div className="text-md flex flex-col gap-y-2">
+        <Image src="/images/qrcode.jpg" width={300} height={300} alt="wx: joydragon"  />
+        <span className="text-gray-500 text-sm text-center font-bold">扫一扫上面的二维码图案，加我为朋友</span>
+      </div>
+    </section>
+  )
+}
